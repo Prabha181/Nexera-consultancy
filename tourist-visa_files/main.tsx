34b20cdce4712b03b749bc3903405496491ebf15
Nexera <nexera@gmail.com>
2025-09-10 15:54:31 +0530

    first commit

---
+++ tourist-visa_files/main.tsx
@@ -0,0 +1,18 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=8b3205ec"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=8b3205ec"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=8b3205ec"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import App from "/client/App.tsx";
+import "/client/global.css";
+ReactDOM.createRoot(document.getElementById('root')).render(/*#__PURE__*/ _jsxDEV(React.StrictMode, {
+    children: /*#__PURE__*/ _jsxDEV(App, {}, void 0, false, {
+        fileName: "C:/Users/nextgen/Pictures/zainth-hub/client/main.tsx",
+        lineNumber: 8,
+        columnNumber: 5
+    }, this)
+}, void 0, false, {
+    fileName: "C:/Users/nextgen/Pictures/zainth-hub/client/main.tsx",
+    lineNumber: 7,
+    columnNumber: 3
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4udHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tICdyZWFjdCdcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20vY2xpZW50J1xuaW1wb3J0IEFwcCBmcm9tICcuL0FwcC50c3gnXG5pbXBvcnQgJy4vZ2xvYmFsLmNzcydcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpISkucmVuZGVyKFxuICA8UmVhY3QuU3RyaWN0TW9kZT5cbiAgICA8QXBwIC8+XG4gIDwvUmVhY3QuU3RyaWN0TW9kZT4sXG4pXG4iXSwibmFtZXMiOlsiUmVhY3QiLCJSZWFjdERPTSIsIkFwcCIsImNyZWF0ZVJvb3QiLCJkb2N1bWVudCIsImdldEVsZW1lbnRCeUlkIiwicmVuZGVyIiwiU3RyaWN0TW9kZSJdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU9BLFdBQVcsUUFBTztBQUN6QixPQUFPQyxjQUFjLG1CQUFrQjtBQUN2QyxPQUFPQyxTQUFTLFlBQVc7QUFDM0IsT0FBTyxlQUFjO0FBRXJCRCxTQUFTRSxVQUFVLENBQUNDLFNBQVNDLGNBQWMsQ0FBQyxTQUFVQyxNQUFNLGVBQzFELFFBQUNOLE1BQU1PLFVBQVU7Y0FDZixjQUFBLFFBQUNMIn0=
